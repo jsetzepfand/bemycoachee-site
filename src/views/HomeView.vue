36a788df10011f14a92195fa49d3c4d3dffaf2be
Jörg Setzepfand <jorg_setzepfand@singular.uk>
2025-09-21 11:26:10 +0200

    AI-change step 4, added booking widget

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
     <HeroSection />
+    <BookingWidget />
   </div>
 </template>
 
 <script setup lang="ts">
 import HeroSection from '@/components/HeroSection.vue'
+import BookingWidget from '@/components/BookingWidget.vue'
 </script>
 
 <style scoped>
